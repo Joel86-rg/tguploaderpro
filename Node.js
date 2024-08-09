cb5e1234c4c1d20cae1edc42554e326def6f41eb
Joel86-rg <gdiazlopez66@gmail.com>
2024-08-09 02:20:38 -0400

    Node.js

--- Node.js
+++ Node.js
@@ -1,5 +1,4 @@
 const port = process.env.PORT || 3000;
-
 app.listen(port, () => {
-  console.log(`Servidor en funcionamiento en el puerto ${port}`);
+  console.log(`Servidor en ejecución en el puerto ${port}`);
 });
